c0c5ce0b8182677db189c50ee3e3a7173e880203
Thiago Farias <thiagofarias99@hotmail.com>
2020-04-14 15:02:18 -0300

    Checkmate logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -20,7 +20,7 @@ public class Program {
         Scanner sc = new Scanner(System.in);
         List<ChessPiece> captured = new ArrayList<>();
 
-        while (true) {
+        while (!chessMatch.getCheckMate()) {
 
             try {
                 UI.clearScreen();
@@ -57,6 +57,9 @@ public class Program {
             }
 
         }
+
+        UI.clearScreen();
+        UI.printMatch(chessMatch, captured);
 
     }
 
